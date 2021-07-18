c3ca2f552f39d415d22f9710593d0afac4d12509
Razjel <65457205+Razjelx@users.noreply.github.com>
2021-07-18 19:02:32 +0200

    Upload

---
+++ part11-Part11_07.BiggerClassDiagram/src/main/java/E.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Razjel
+ */
+import java.util.ArrayList;
+public class E {
+    private String name;
+    private ArrayList<C> list;
+}
